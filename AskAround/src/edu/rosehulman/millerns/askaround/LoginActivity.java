1352a0f619bba40d2679eba5c779e0a59d065e21
millerns <millerns@rose-hulman.edu>
2015-01-25 20:14:08 -0500

    Sprint 1 - updated layouts and created activity stubs.

--- AskAround/src/edu/rosehulman/millerns/askaround/LoginActivity.java
+++ AskAround/src/edu/rosehulman/millerns/askaround/LoginActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class CurrentQuestionActivity extends Activity {
+public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_current_question);
+		setContentView(R.layout.activity_login);
 	}
 }
