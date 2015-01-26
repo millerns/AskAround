61475f8ac537667bf347c78281542dfa37ce8995
millerns <millerns@rose-hulman.edu>
2015-01-25 19:27:39 -0500

    Sprint 1 - Layouts

---
+++ AskAround/src/edu/rosehulman/millerns/askaround/CurrentQuestionActivity.java
@@ -0,0 +1,15 @@
+package edu.rosehulman.millerns.askaround;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CurrentQuestionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.result_with_options);
+	}
+}
