35d112fa1fdb6af0d13c8637a1be296690e04026
AlphaDing <dingx@rose-hulman.edu>
2015-02-15 17:24:36 -0500

    loading question from the backend and be able to update with user chocies

--- AskAround/src/edu/rosehulman/millerns/askaround/CreateQuestionActivity.java
+++ AskAround/src/edu/rosehulman/millerns/askaround/CreateQuestionActivity.java
@@ -24,14 +24,13 @@ import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.json.gson.GsonFactory;
 
 public class CreateQuestionActivity extends Activity {
-	
+
 	private static final String AA = "AA";
+
 	private enum QuestionType {
-		COMMENTS_ONLY_QUESTION,
-		SINGLE_CHOICE_QUESTION,
-		MULTIPLE_CHOICE_QUESTION
+		COMMENTS_ONLY_QUESTION, SINGLE_CHOICE_QUESTION, MULTIPLE_CHOICE_QUESTION
 	}
-	
+
 	private ArrayList<String> mNewOptions;
 	private EditText questionContent;
 	private ListView newOptionListView;
@@ -45,8 +44,9 @@ public class CreateQuestionActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_question);
-		
-		Questions.Builder builder = new Questions.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), null);
+
+		Questions.Builder builder = new Questions.Builder(
+				AndroidHttp.newCompatibleTransport(), new GsonFactory(), null);
 		mService = builder.build();
 
 		questionContent = (EditText) findViewById(R.id.question_content);
@@ -55,40 +55,41 @@ public class CreateQuestionActivity extends Activity {
 		newOptionListView = (ListView) findViewById(R.id.answer_choices_list_view);
 		mNewOptions = new ArrayList<String>();
 		mNewOptionAdapter = new CreateOptionListViewAdapter(this, mNewOptions);
-		
+
 		setupOptionsListView();
-		setupQuestionTypeButtons();	
+		setupQuestionTypeButtons();
 		setupAddOptionsButton();
 		setupSubmitQuestionButton();
-		
+
 	}
 
 	private void setupOptionsListView() {
 		newOptionListView.setAdapter(mNewOptionAdapter);
-		newOptionListView.setOnItemLongClickListener(new OnItemLongClickListener() {
+		newOptionListView
+				.setOnItemLongClickListener(new OnItemLongClickListener() {
 
-			@Override
-			public boolean onItemLongClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				deleteNewOption(position);
-				return true;
-			}
-		});
-	}	
-	
+					@Override
+					public boolean onItemLongClick(AdapterView<?> parent,
+							View view, int position, long id) {
+						deleteNewOption(position);
+						return true;
+					}
+				});
+	}
+
 	private void setupAddOptionsButton() {
 		addOptionButton.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				addNewOption();
 			}
 		});
 	}
-	
+
 	public void setupSubmitQuestionButton() {
 		submitQuestionButton.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Question newQuestion = new Question();
@@ -112,65 +113,74 @@ public class CreateQuestionActivity extends Activity {
 			}
 		});
 	}
-	
-	private void setupQuestionTypeButtons() {		
+
+	private void setupQuestionTypeButtons() {
 		final Button commentOnlyButton = (Button) findViewById(R.id.comments_only_type);
 		final Button singleChoiceButton = (Button) findViewById(R.id.pick_one_type);
-		final Button multipleChoiceButton = (Button) findViewById(R.id.pick_any_type);		
+		final Button multipleChoiceButton = (Button) findViewById(R.id.pick_any_type);
 		final TextView answerChoice = (TextView) findViewById(R.id.answer_choices);
-		
+
 		commentOnlyButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				commentOnlyButton.setBackgroundColor(getResources().getColor(R.color.blue));
-				singleChoiceButton.setBackgroundColor(getResources().getColor(R.color.white));
-				multipleChoiceButton.setBackgroundColor(getResources().getColor(R.color.white));
+				commentOnlyButton.setBackgroundColor(getResources().getColor(
+						R.color.blue));
+				singleChoiceButton.setBackgroundColor(getResources().getColor(
+						R.color.white));
+				multipleChoiceButton.setBackgroundColor(getResources()
+						.getColor(R.color.white));
 				answerChoice.setVisibility(View.GONE);
 				addOptionButton.setVisibility(View.GONE);
 				newOptionListView.setVisibility(View.GONE);
 				mQuestionType = QuestionType.COMMENTS_ONLY_QUESTION;
 			}
 		});
-		
+
 		singleChoiceButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				commentOnlyButton.setBackgroundColor(getResources().getColor(R.color.white));
-				singleChoiceButton.setBackgroundColor(getResources().getColor(R.color.blue));
-				multipleChoiceButton.setBackgroundColor(getResources().getColor(R.color.white));
+				commentOnlyButton.setBackgroundColor(getResources().getColor(
+						R.color.white));
+				singleChoiceButton.setBackgroundColor(getResources().getColor(
+						R.color.blue));
+				multipleChoiceButton.setBackgroundColor(getResources()
+						.getColor(R.color.white));
 				answerChoice.setVisibility(View.VISIBLE);
 				addOptionButton.setVisibility(View.VISIBLE);
 				newOptionListView.setVisibility(View.VISIBLE);
 				mQuestionType = QuestionType.SINGLE_CHOICE_QUESTION;
 			}
 		});
-		
+
 		multipleChoiceButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				commentOnlyButton.setBackgroundColor(getResources().getColor(R.color.white));
-				singleChoiceButton.setBackgroundColor(getResources().getColor(R.color.white));
-				multipleChoiceButton.setBackgroundColor(getResources().getColor(R.color.blue));
+				commentOnlyButton.setBackgroundColor(getResources().getColor(
+						R.color.white));
+				singleChoiceButton.setBackgroundColor(getResources().getColor(
+						R.color.white));
+				multipleChoiceButton.setBackgroundColor(getResources()
+						.getColor(R.color.blue));
 				answerChoice.setVisibility(View.VISIBLE);
 				addOptionButton.setVisibility(View.VISIBLE);
 				newOptionListView.setVisibility(View.VISIBLE);
 				mQuestionType = QuestionType.MULTIPLE_CHOICE_QUESTION;
 			}
 		});
-		
+
 	}
-	
+
 	private void addNewOption() {
 		DialogFragment df = new DialogFragment() {
 			@Override
 			public Dialog onCreateDialog(Bundle savedInstanceState) {
 				AlertDialog.Builder builder = new AlertDialog.Builder(
 						getActivity());
-				
+
 				builder.setTitle(R.string.add_answer_choice);
 				final EditText input = new EditText(getActivity());
 				builder.setView(input);
-				
+
 				builder.setPositiveButton(android.R.string.ok,
 						new DialogInterface.OnClickListener() {
 
@@ -192,7 +202,7 @@ public class CreateQuestionActivity extends Activity {
 
 		df.show(getFragmentManager(), "add new option");
 	}
-	
+
 	private void deleteNewOption(final int position) {
 		DialogFragment df = new DialogFragment() {
 			@Override
@@ -221,37 +231,36 @@ public class CreateQuestionActivity extends Activity {
 		};
 
 		df.show(getFragmentManager(), "delete new option");
-		
+
 	}
-	
+
 	private void insertQuestionTask(Question currentQuestion) {
 		new InsertQuestionTask().execute(currentQuestion);
 	}
-	
+
 	class InsertQuestionTask extends AsyncTask<Question, Void, Question> {
 
 		@Override
 		protected Question doInBackground(Question... questions) {
 			Question returnedQuestion = null;
 			try {
-				returnedQuestion = mService.question().insert(questions[0]).execute();
+				returnedQuestion = mService.question().insert(questions[0])
+						.execute();
 			} catch (IOException e) {
 				Log.e(AA, "Failed inserting: " + e);
 			}
 			return returnedQuestion;
 		}
-		
+
 		@Override
 		protected void onPostExecute(Question result) {
 			super.onPostExecute(result);
 			if (result == null) {
 				Log.e(AA, "Failed inserting, result is null");
 			}
-			
+
 		}
-		
-		
+
 	}
-	
-	
+
 }
--- AskAround/src/edu/rosehulman/millerns/askaround/CurrentQuestionFragment.java
+++ AskAround/src/edu/rosehulman/millerns/askaround/CurrentQuestionFragment.java
@@ -1,19 +1,25 @@
 package edu.rosehulman.millerns.askaround;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import com.appspot.dingx_askaround.questions.Questions;
+import com.appspot.dingx_askaround.questions.Questions.Question.List;
+import com.appspot.dingx_askaround.questions.model.QuestionCollection;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.json.gson.GsonFactory;
+import com.appspot.dingx_askaround.questions.model.Question;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.content.DialogInterface;
-import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.webkit.WebView.FindListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -27,14 +33,18 @@ public class CurrentQuestionFragment extends Fragment {
 	private Question currentQuestion;
 	private View v;
 	private int selectedOptionIndex = -1;
-	private String comment;
+	private String newComment;
+	private Questions mService;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		v = inflater.inflate(R.layout.activity_current_question, container, false);
 		
-		currentQuestion = generateNextQuestion();
+		Questions.Builder builder = new Questions.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(), null);
+		mService = builder.build();
+		
+		generateNextQuestion();
 		
 		TextView questionContent = (TextView) v.findViewById(R.id.question);
 		questionContent.setText(currentQuestion.getContent());
@@ -47,19 +57,35 @@ public class CurrentQuestionFragment extends Fragment {
 			
 			@Override
 			public void onClick(View v) {	
-				comment = commentsView.getText().toString();
+				newComment = commentsView.getText().toString();
+				java.util.List<String> comments = currentQuestion.getComments();
+				java.util.List<String> options = currentQuestion.getOptions();
+				java.util.List<Long> votes = currentQuestion.getVotes();
+				String content = currentQuestion.getContent();
+				String questionType = currentQuestion.getQuestionType();
+				String entityKey = currentQuestion.getEntityKey();
 				
 				if (selectedOptionIndex == -1) {
 					popErrorDialog();
 				} else {
-					currentQuestion.addVote(selectedOptionIndex);
-					Log.d("AA", "you choose option: " + currentQuestion.getQuestionOptions().get(selectedOptionIndex));
+					votes.set(selectedOptionIndex, votes.get(selectedOptionIndex) + 1);
+					Log.d("AA", "you choose option: " + options.get(selectedOptionIndex));
 					
-					if (comment != null){
-						currentQuestion.addComment(comment);
-						Log.d("AA", "Comment: " + comment);
+					if (newComment != null){
+						comments.add(newComment);
+						Log.d("AA", "Comment: " + newComment);
 					}
-				}				
+				}	
+				
+				Question updatedQuestion = new Question();
+				updatedQuestion.setContent(content);
+				updatedQuestion.setComments(comments);
+				updatedQuestion.setOptions(options);
+				updatedQuestion.setVotes(votes);
+				updatedQuestion.setQuestionType(questionType);
+				updatedQuestion.setEntityKey(entityKey);
+				
+				updateQuestion(updatedQuestion);
 			}
 		});
 		
@@ -69,7 +95,7 @@ public class CurrentQuestionFragment extends Fragment {
 	private void loadOptions(final Question question) {
 		ListView optionListView = (ListView) v.findViewById(R.id.current_question_votes_list_view);
 		
-		ArrayList optionList = question.getQuestionOptions(); 
+		ArrayList<String> optionList = (ArrayList<String>) question.getOptions(); 
 		
 		//for testing
 //		ArrayList<String> optionList = new ArrayList<String>();
@@ -117,12 +143,90 @@ public class CurrentQuestionFragment extends Fragment {
 		df.show(getFragmentManager(), "option error dialog");
 	}
 	
-	private Question generateNextQuestion() {
-		SingleChoiceQuestion question = new SingleChoiceQuestion(10, "hello question 1");
-		question.addOption("so you are option 1");
-		question.addOption("I am option 2");
-		question.addOption("please choose me");
-		
-		return question;
+	private void generateNextQuestion() {
+//		SingleChoiceQuestion question = new SingleChoiceQuestion(10, "hello question 1");
+//		question.addOption("so you are option 1");
+//		question.addOption("I am option 2");
+//		question.addOption("please choose me");
+//		
+//		return question;
+		new QueryForTask().execute();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Log.d("AA", "finish executing query");
 	}
+	
+	private void updateQuestion(Question newQuestion) {
+		new InsertQuestionTask().execute(newQuestion);
+	}
+	
+	
+	private class QueryForTask extends AsyncTask<Void, Void, QuestionCollection> {
+
+		@Override
+		protected QuestionCollection doInBackground(Void... params) {
+			// TODO Auto-generated method stub
+			Log.d("AA", "start generating new question");
+
+			QuestionCollection questions = null;
+			try {
+				List query = mService.question().list();
+				query.setOrder("create_date");
+				query.setLimit(50L);
+				questions = query.execute();
+				Log.d("AA", "got new questions: " + questions.size());
+				currentQuestion = questions.getItems().get(0);
+				Log.d("AA", "current items: " + questions.getItems().get(0).getContent());
+				
+			} catch (IOException e) {
+				Log.e("AA", "Failed loading: " + e);
+			}
+			
+			Log.d("AA", "About to return question collections");
+			return questions;
+		}
+		
+		@Override
+		protected void onPostExecute(QuestionCollection result) {
+			super.onPostExecute(result);
+			Log.d("AA", "In post execute");
+			
+			if (result == null) {
+				Log.d("AA", "Failed loading, result is null");
+				return;
+			}
+			
+			
+			
+		}
+	}
+	
+	private class InsertQuestionTask extends AsyncTask<Question, Void, Question> {
+
+		@Override
+		protected Question doInBackground(Question... questions) {
+			Question returnedQuestion = null;
+			try {
+				returnedQuestion = mService.question().insert(questions[0]).execute();
+			} catch (IOException e) {
+				Log.e("AA", "Failed inserting: " + e);
+			}
+			return returnedQuestion;
+		}
+		
+		@Override
+		protected void onPostExecute(Question result) {
+			super.onPostExecute(result);
+			if (result == null) {
+				Log.e("AA", "Failed inserting, result is null");
+			}	
+		}	
+	}
+	
+	
+	
+	
 }
